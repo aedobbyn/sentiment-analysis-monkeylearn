52da33625d3ea4af2d84aa9d6988b38febd2e8eb
aedobbyn <adobbs11@gmail.com>
2018-05-11 18:06:19 -0500

    unnest the raw response

--- topics/classify_topics.R
+++ topics/classify_topics.R
@@ -9,29 +9,23 @@ reviews_with_subratings <- read_csv(here("data", "derived", "all_reviews_slack.c
 reviews_with_subratings_unnested <- read_csv(here("data", "derived", "capterra_slack_reviews_with_subratings_unnested.csv"))
 
 
-replace_nulls <- function(e, rep) {
-  if (length(x) == 0 || is.null(x)) {
-    
-  }
-}
 
 replacement <- tribble(
   ~category_id, ~probability, ~label,
   NA_character_, NA_character_, NA_character_
 ) %>% list()
 
-sample_topics <- 
+sample_topics_raw <- 
   monkey_classify(reviews_with_subratings[1:3, ], col = content,
-                  classifier_id = classifier_id, unnest = FALSE) %>% 
+                  classifier_id = classifier_id, unnest = FALSE) 
+
+
+sample_topics_unnested <- sample_topics_raw %>% 
+  rowwise() %>% 
   mutate(
-    res = res %>% map(dobtools::replace_x, replacement = replacement) # %>% list()
-  )
+    res = ifelse(length(res)[[1]] == 0, replacement, res) 
+  ) %>% unnest(res)
 
-
-foo[which(length(foo) == 0)] <- replacement
-
-
-sample_topics %>% unnest()
 
 
 
