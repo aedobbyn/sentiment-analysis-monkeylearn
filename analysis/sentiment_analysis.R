0f6621587f3283461417ced8829e61111ee5cea1
aedobbyn <adobbs11@gmail.com>
2018-05-18 19:33:20 -0500

    turn sentiment numeric and get mean sentiment per category

--- analysis/sentiment_analysis.R
+++ analysis/sentiment_analysis.R
@@ -12,17 +12,57 @@ dat_unsplit <-
   rename(content = Text,
          sentiment = Sentiment,
          probability_sentiment = Probability,
-         categories = `Categories (Full-text)`,
-         probability_unit = `Probabilities (Full-text)`
+         categories_full = `Categories (Full-text)`,
+         probability_unit_full = `Probabilities (Full-text)`
          )
 
 dat <- 
   dat_unsplit %>% 
   mutate(
-    categories_nested = str_split(categories, ":"),
-    probability_unit_nested = str_split(probability_unit, ":")
+    category = str_split(categories_full, ":"),
+    probability_unit = str_split(probability_unit_full, ":")
   ) %>% 
-  unnest()
+  unnest() %>% 
+  select(-ends_with("full")) %>% 
+  rowwise() %>% 
+  mutate(
+    sentiment_num = switch(sentiment, 
+                           "Negative" = -1,
+                           "Neutral" = 0,
+                           "Positive" = 1)
+  ) %>% 
+  ungroup()
+
+dat_clean <-
+  dat %>% 
+  filter(!is.na(probability_unit) & !is.na(probability_unit) & 
+           category != "None" &
+           probability_sentiment > 0.55 & probability_unit > 0.55)
+
+
+# Summarise sentiment by category 
+sentiment_by_category <- 
+  dat_clean %>% 
+  group_by(category) %>% 
+  summarise(
+    mean_sentiment = mean(sentiment_num)
+  ) %>% 
+  arrange(mean_sentiment)
+
+# Breakdown by category
+ggplot(dat_clean) +
+  geom_bar(aes(category))
+
+
+
+
+ggplot(sentiment_by_category) +
+  geom_bar(aes(category, mean_sentiment), stat = "identity")
+
+
+
+
+
 
 
 
